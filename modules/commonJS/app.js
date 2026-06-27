f7d773cac15176199b651bc5668199a07e411f41
krrish-kohli <krrishkohli15@gmail.com>
2026-06-27 16:35:21 +0530

    new files added

---
+++ modules/commonJS/app.js
@@ -0,0 +1,13 @@
+// // Import the entire math module
+// const math = require("./math");
+
+// const addResults = math.add(10, 20);
+// const subtractResults = math.subtract(10, 20);
+
+// console.log(addResults);
+// console.log(subtractResults);
+
+const { add, subtract } = require("./math");
+
+console.log(add(40, 20));
+console.log(subtract(100, 20));
